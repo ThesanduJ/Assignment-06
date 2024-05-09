aa59b34351260ce6dcc43a5d40af1bd2266cd205
ThesanduJ <thesandu1602500@gmail.com>
2024-05-09 23:05:39 +0530

    create JS file

---
+++ assets/JS/js.js
@@ -0,0 +1,19 @@
+$('#login-group').css({display: 'block'});
+$('#dash-group').css({display: 'none'});
+$('#cus-group').css({display: 'none'});
+$('#item-group').css({display: 'none'});
+$('#placeOrder-group').css({display: 'none'});
+$('#viewOrder-group').css({display: 'none'});
+$('#sign-up-group').css({display: 'none'});
+
+
+// $('#nav-student').on('click', () => {
+//     $('#student-section').css({display: 'block'});
+//     $('#course-section').css({display: 'none'});
+// });
+// //
+// // // course nav management
+// // $('#nav-course').on('click', () => {
+// //     $('#student-section').css({display: 'none'});
+// //     $('#course-section').css({display: 'block'});
+// // });
