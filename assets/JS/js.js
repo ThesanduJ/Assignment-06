d924915633a8ed770b39185d7295950bf7d4304e
ThesanduJ <thesandu1602500@gmail.com>
2024-05-10 21:48:24 +0530

    add S-P-A for sign-up

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -364,6 +364,17 @@ $('#viewOrder-placeOrder').on('click', () => {
 
 });
 
+//-------------------------------sign-up-----------------------------------------------
 
+$('#createAccount').on('click', () => {
+    $('#login-group').css({display: 'block'});
+    $('#dash-group').css({display: 'none'});
+    $('#cus-group').css({display: 'none'});
+    $('#item-group').css({display: 'none'});
+    $('#placeOrder-group').css({display: 'none'});
+    $('#viewOrder-group').css({display: 'none'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
 
 
