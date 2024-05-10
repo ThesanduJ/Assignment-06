95514e6ef12ed39151b083bc245e6fde09978c52
ThesanduJ <thesandu1602500@gmail.com>
2024-05-10 21:48:05 +0530

    add S-P-A for view order

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -306,3 +306,64 @@ $('#placeOrder-placeOrder').on('click', () => {
 
 
 
+//----------------------view Order---------------------------------------------------
+
+$('#viewOrder-logo').on('click', () => {
+    $('#login-group').css({display: 'none'});
+    $('#dash-group').css({display: 'block'});
+    $('#cus-group').css({display: 'none'});
+    $('#item-group').css({display: 'none'});
+    $('#placeOrder-group').css({display: 'none'});
+    $('#viewOrder-group').css({display: 'none'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
+
+$('#viewOrder-customer').on('click', () => {
+    $('#login-group').css({display: 'none'});
+    $('#dash-group').css({display: 'none'});
+    $('#cus-group').css({display: 'block'});
+    $('#item-group').css({display: 'none'});
+    $('#placeOrder-group').css({display: 'none'});
+    $('#viewOrder-group').css({display: 'none'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
+
+
+$('#viewOrder-item').on('click', () => {
+    $('#login-group').css({display: 'none'});
+    $('#dash-group').css({display: 'none'});
+    $('#cus-group').css({display: 'none'});
+    $('#item-group').css({display: 'block'});
+    $('#placeOrder-group').css({display: 'none'});
+    $('#viewOrder-group').css({display: 'none'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
+
+$('#viewOrder-view').on('click', () => {
+    $('#login-group').css({display: 'none'});
+    $('#dash-group').css({display: 'none'});
+    $('#cus-group').css({display: 'none'});
+    $('#item-group').css({display: 'none'});
+    $('#placeOrder-group').css({display: 'none'});
+    $('#viewOrder-group').css({display: 'block'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
+
+$('#viewOrder-placeOrder').on('click', () => {
+    $('#login-group').css({display: 'none'});
+    $('#dash-group').css({display: 'none'});
+    $('#cus-group').css({display: 'none'});
+    $('#item-group').css({display: 'none'});
+    $('#placeOrder-group').css({display: 'block'});
+    $('#viewOrder-group').css({display: 'none'});
+    $('#sign-up-group').css({display: 'none'});
+
+});
+
+
+
+
