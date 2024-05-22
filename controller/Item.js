e5adf730c2e6a01db9727e7ef07133908f3508bf
ThesanduJ <thesandu1602500@gmail.com>
2024-05-22 16:24:35 +0530

    add save function

--- controller/Item.js
+++ controller/Item.js
@@ -1,3 +1,6 @@
+import db from "../model/ItemModel.js";
+import {items} from "../db/db.js";
+
 $('#item-btnExit').on('click', () => {
     $('#login-group').css({display: 'block'});
     $('#dash-group').css({display: 'none'});
@@ -63,4 +66,37 @@ $('#item-placeOrder').on('click', () => {
     $('#viewOrder-group').css({display: 'none'});
     $('#sign-up-group').css({display: 'none'});
 
+});
+function loadTable() {
+
+    $("#item-table-body").empty();
+
+    items.map((item, index) => {
+        let record = `<tr>
+                <td class="item-id">${item.itemCode}</td>
+                <td class="item-description">${item.itemName}</td>
+                <td class="item-price">${item.price}</td>
+                <td class="item-qty">${item.itemQty}</td>
+            </tr>`;
+        $("#item-table-body").append(record);
+    });
+}
+$("#item-btnSave").on('click', () => {
+
+    var itemID = $('.item-ids').val();
+    var itemDes = $('.item-name').val();
+    var itemPrice = $('.item-prices').val();
+    var itemQty = $('.item-quantities').val();
+
+    // create an object
+    let itemsDetails = {
+        itemCode: itemID,
+        itemName: itemDes,
+        price: itemPrice,
+        itemQty: itemQty
+    }
+    // push to the array
+    items.push(itemsDetails);
+
+    loadTable();
 });
