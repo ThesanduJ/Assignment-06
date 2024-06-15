dc72ddb92220e065588586c678a4c756d33fd116
ThesanduJ <thesandu1602500@gmail.com>
2024-06-16 02:24:50 +0530

    create new variable and methods

--- controller/PlaceOrder.js
+++ controller/PlaceOrder.js
@@ -1,5 +1,12 @@
 import {customers, items} from "../db/db.js";
 
+var price;
+var itemCount = null;
+var discountAmount;
+var paidPrice = null;
+var total;
+var subTotal;
+
 $('#placeOrder-btnExit').on('click', () => {
     $('#login-group').css({display: 'block'});
     $('#dash-group').css({display: 'none'});
@@ -68,61 +75,85 @@ $('#placeOrder-placeOrder').on('click', () => {
 });
 
 $(".placeOrder-inp-01").val("O-01");
-$(document).ready(function() {
-    var count=2;
-    $("#placeOrder-btnPurchase").click(function (){
-        if (count<=0) {
-            count=1;
+$(document).ready(function () {
+    var count = 2;
+    $("#placeOrder-btnPurchase").click(function () {
+        if (count <= 0) {
+            count = 1;
         }
-        $(".placeOrder-inp-01").val("O-0"+count);
+        $(".placeOrder-inp-01").val("O-0" + count);
         ++count;
         console.log(count);
     });
 
-    $("#placeOrder-btnRemove").click(function (){
+    $("#placeOrder-btnRemove").click(function () {
         --count;
-        if (count>0){
-            $(".placeOrder-inp-01").val("O-0"+count);
-        }else {
+        if (count > 0) {
+            $(".placeOrder-inp-01").val("O-0" + count);
+        } else {
             swal("Error!", "Can't change the Order ID!", "error");
         }
         console.log(count);
     });
 });
 $("document").ready(function () {
-    $(".placeOrder-inp-02").on('input',function () {
-        var inputV=$(this).val();
-        var name=null;
-        for (let i = 0; i <customers.length; i++) {
-            if (inputV===customers[i].number){
-                name=customers[i].name;
+    $(".placeOrder-inp-02").on('input', function () {
+        var inputV = $(this).val();
+        var name = null;
+        for (let i = 0; i < customers.length; i++) {
+            if (inputV === customers[i].number) {
+                name = customers[i].name;
                 $(".placeOrder-inp-03").val(name);
                 console.log(name);
                 break;
-            }else {
+            } else {
                 $(".placeOrder-inp-03").val("");
             }
         }
     });
 });
 $("document").ready(function () {
-    $(".placeOrder-inp-04").on('input',function () {
-        var inputV=$(this).val();
-        var itemName=null;
-        for (let i = 0; i <items.length; i++) {
-            if (inputV===items[i].itemCode){
-                itemName=items[i].itemName;
+    $(".placeOrder-inp-04").on('input', function () {
+        var inputV = $(this).val();
+        var itemName = null;
+        for (let i = 0; i < items.length; i++) {
+            if (inputV === items[i].itemCode) {
+                itemName = items[i].itemName;
+                itemCount = items[i].itemQty;
+                price = items[i].price;
                 $(".placeOrder-inp-05").val(itemName);
+                $(".placeOrder-inp-06").val(itemCount);
                 console.log(itemName);
+                console.log(itemCount);
                 break;
-            }else {
+            } else {
                 $(".placeOrder-inp-05").val("");
+                $(".placeOrder-inp-06").val("");
             }
         }
     });
 });
-
-
+$("#placeOrder-btnAdd").on('click', () => {
+    swal("Successfully Added!", "!", "success");
+    total = price * itemCount;
+    $("#total").val(total);
+});
+$("document").ready(function () {
+    $(".placeOrder-inp-11").on('input', function () {
+        discountAmount = $(".placeOrder-inp-11").val();
+        $("#sub-total").text(total - discountAmount)
+    });
+});
+$("document").ready(function () {
+    $(".placeOrder-inp-11").on('input', function () {
+        discountAmount = $(".placeOrder-inp-11").val();
+        subTotal = $("#sub-total").val(total - discountAmount);
+        // subTotal = $("#sub-total").val(total - discountAmount);
+        paidPrice = $(".placeOrder-inp-12").val();
+        // $(".placeOrder-inp-13").val(subTotal - paidPrice)
+        console.log(subTotal - paidPrice);
+    });
+});
 
 
 
